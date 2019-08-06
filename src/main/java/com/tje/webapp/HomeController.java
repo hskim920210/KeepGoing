d2eda471c8c0e331d39c7cd3e62cc382b741375f
KimTH <KimTH@ex.ex>
2019-08-06 09:49:31 +0900

    190806_KimTH

--- src/main/java/com/tje/webapp/HomeController.java
+++ src/main/java/com/tje/webapp/HomeController.java
@@ -76,8 +76,8 @@ public class HomeController {
 		List<SimpleBoardReviewView> review_list = (List<SimpleBoardReviewView>)hrService.service();
 		model.addAttribute("reviewList", review_list);
 		
-		List<SimpleBoardFreeView> simple_list = (List<SimpleBoardFreeView>)hfService.service();
-		model.addAttribute("simpleList", simple_list);
+//		List<SimpleBoardFreeView> simple_list = (List<SimpleBoardFreeView>)hfService.service();
+//		model.addAttribute("simpleList", simple_list);
 		
 		return "home";
 	}
